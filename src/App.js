905f4ead7da488d188fe964f554a74fbbb290f93
vlad.moraru@tech-off.com <vlad.moraru@tech-off.com>
2022-06-27 09:24:50 +0300

    react 3d menu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <ul>
+                <li style={{zIndex:6}}><a  href="#">Home</a></li>
+                <li style={{zIndex:5}}><a href="#">About</a></li>
+                <li style={{zIndex:4}}><a href="#">Services</a></li>
+                <li style={{zIndex:3}}><a href="#">Portofolio</a></li>
+                <li style={{zIndex:2}}><a href="#">Our Team</a></li>
+                <li style={{zIndex:1}}><a href="#">Contact</a></li>
+            </ul>
+        </div>
+    );
 }
 
 export default App;
